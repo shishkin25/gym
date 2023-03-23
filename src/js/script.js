3baf6914038e23dcd9c5ea3457fa49103beaf597
Artem <example@gmail.com>
2023-03-23 16:43:49 +0300

    all functionality is ready, left only adaptivity

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,10 @@ const pricesSwitch = body.querySelector('.prices__switch input');
 const discount = body.querySelector('.prices__discount');
 const coaches = body.querySelectorAll('.team__coach');
 const links = body.querySelectorAll('.team__coach-links');
+const buttonWrapper = body.querySelectorAll('.button__wrapper');
+const clients = body.querySelectorAll('.clients__images-wrapper');
+const contacts = body.querySelectorAll('.client__contacts');
+let clickCounter = 0;
 
 btns.forEach((item) => {
     item.addEventListener('click', (e) => {
@@ -52,3 +56,45 @@ coaches.forEach((coach, index) => {
         timerId = setTimeout(RemoveAndAddClass, 100, links[index], 'showDf', 'hide');
     });
 })
+
+// заменая белой стрелки на красную при наведении на кнопку
+buttonWrapper.forEach((element) => {
+    const arrows = element.querySelectorAll('img');
+    element.addEventListener('mouseover', (e) => {
+        arrows[0].classList.add('hide');
+        arrows[1].classList.remove('hide');
+    });
+    element.addEventListener('mouseout', (e) => {
+        arrows[0].classList.remove('hide');
+        arrows[1].classList.add('hide');
+    });
+})
+
+
+
+const buttonBack = buttonWrapper[0];
+const buttonForward = buttonWrapper[1];
+buttonWrapper.forEach((currentButton) => {
+    currentButton.addEventListener('click', (e) => {
+        clients[clickCounter].classList.remove('choosen-client');
+        contacts[clickCounter].classList.add('hide');
+        if (currentButton === buttonBack) {
+            if (clickCounter == 0) {
+                clickCounter = 3;
+            } else {
+                clickCounter--;
+            }
+        }
+        
+        if (currentButton === buttonForward) {
+            if (clickCounter == 3) {
+                clickCounter = 0;
+            } else {
+                clickCounter++;
+            }
+        }        
+
+        clients[clickCounter].classList.add('choosen-client');
+        contacts[clickCounter].classList.remove('hide');
+    });
+});
